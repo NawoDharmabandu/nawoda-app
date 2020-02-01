229648ae1c37ece4f5f7231843ce496b036db58a
Nawoda Dharmabandu <ptnawoda@gmail.com>
2020-02-01 15:10:39 +0530

    Create ToDo.js

---
+++ ToDo.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, StatusBar, TextInput, TouchableOpacity,Button} from 'react-native';
+import { LinearGradient} from 'expo-linear-gradient';
+
+
+export default class App extends React.Component {
+
+  state = {
+    inputValue: '',
+    todos: []
+  };
+
+  changeText = value=> {
+    this.setState({
+      inputValue: value
+    });
+  };
+
+  addItem = () => {
+    if(this.state.inputValue !== ''){
+      this.setState(prevState=> {
+        const newToDo = {
+          title: this.state.inputValue,
+          createdAt: Date.now(),
+        };
+
+        var todos = this.state.todos.concat(newToDo);
+
+        this.setState({
+          todos: todos,
+        });
+      });
+    }
+  };
+
+  render() {
+    const todos = this.state.todos.reverse().map((todo,key)=>
+    <View style={{ flexDirection: 'row',marginTop: 20}}>
+      <TouchableOpacity style={{
+        width: 20,
+        height: 20,
+        borderRadius: 15,
+        borderWidth: 3,
+        borderColor: 'white',
+        margin: 15
+      }}>
+      </TouchableOpacity>
+
+      <Text style={{paddingLeft: 5, marginTop: 10, fontSize: 28, color: 'white'}}>{todo.title}</Text>
+    </View>
+    );
+    
+
+
+    return (
+      <LinearGradient colors={['#EECF10','#94F5D0']} style={{flex:1}}>
+        <StatusBar barStyle="light-content" />
+
+        <View> 
+          <TextInput
+            style={styles.input}
+            onSubmitEditing={this.addItem}
+            onChangeText={this.changeText}
+            placeholder = "Type here to add ato do :)"
+            value={this.state.inputValue}
+            placeholderTextColor = {'#fff'}
+            multiline={true}
+            autoCapitalize="sentences"
+            underlineColorAndroid = "transparent"
+            selectionColor = {'white'}
+            maxLength = {30}
+            returnKeyType="done"
+            autoCorrect={false}
+            blurOnSubmit={true}
+          />
+        </View>
+
+        <View>
+        {todos}
+        </View>
+
+      </LinearGradient>
+
+      
+    );
+  }
+}
+
+const styles={
+  input:
+  {
+    marginTop: 30,
+    paddingTop:10,
+    paddingRight : 15,
+    paddingLeft: 15,
+    fontSize: 34,
+    color: 'white',
+    fontWeight: '500'
+  },
+};
